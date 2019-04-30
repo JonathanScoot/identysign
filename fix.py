b110ae1d7d7621e720e1ea9ff54e2b6677c937d5
Jonathan <Jonathanelec@163.com>
2019-04-30 09:26:56 +0800

    change image path

--- fix.py
+++ fix.py
@@ -1,11 +1,14 @@
 from aip import AipOcr
+import cv2
 
 def Prepare():
 
     APP_ID = '16116533'
     API_KEY = 'xDzBG3xsbz9ZgeoSb6kTC4M1'
     SECRET_KEY = 'nxBj36bQfIRLgh7U3aIxgoaEYCVtqLQF'
-    image_path = '/Users/jonathan/Desktop/sign9.jpeg'
+    image_path = '/Users/jonathan/Desktop/sign7.jpeg'
+
+
     client = AipOcr(APP_ID, API_KEY, SECRET_KEY)
 
 
@@ -34,22 +37,40 @@ def Prepare():
             formula += i
 
     sign = recresult3[1]
+    rightorwrong = None
     print("识别结果为：\n")
     print(formula)
 
     if sign == '*' and (int(recresult3[0]) * int(recresult3[2])) == int(recresult3[4]):
         print('the result {} is correct √ '.format(recresult3[4]))
+        rightorwrong = 'right'
 
     elif sign == '+' and (int(recresult3[0]) + int(recresult3[2])) == int(recresult3[4]):
         print('the result {} is correct √ '.format(recresult3[4]))
+        rightorwrong = 'right'
 
     elif sign == '-' and (int(recresult3[0]) - int(recresult3[2])) == int(recresult3[4]):
         print('the result {} is correct √ '.format(recresult3[4]))
+        rightorwrong = 'right'
 
     elif sign == '/' and (int(recresult3[0]) / int(recresult3[2])) == int(recresult3[4]):
         print('the result {} is correct √ '.format(recresult3[4]))
+        rightorwrong = 'right'
 
     else:
-        print('the result {} is error × '.format(recresult3[4]))
+        print('the result is error × ')
+        rightorwrong = 'wrong'
+
+    im = cv2.imread(image_path)
+    im = cv2.resize(im, (800, 800))
+
+    text = "{}  {}".format(formula, rightorwrong)
+    cv2.putText(im, text, (100, 100),
+                cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 0, 255), 3)
+
+    cv2.imshow("result", im)
+
+    cv2.waitKey(0)
 
 Prepare()
+
